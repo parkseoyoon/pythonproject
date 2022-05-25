d1e59b9abdfafd266f342434aafacd0a1593e30e
parkseoyoon <seoyoon220@gmail.com>
2022-05-25 09:28:34 +0900

    final

--- day07/oracle_final.py
+++ day07/oracle_final.py
@@ -1,3 +1,33 @@
+# 과일가게 재고 프로그램 구축
+
+# 1. 기능
+# 과일 입력(I), 과일 판매(S), 재고 리스트(L), 품절품목확인(C), 삭제(D), 종료(Q)
+#-괄호 안 영문자를 입력하면 각 기능이 구현되게 만든다
+
+# 2. 입력(I) 
+# 테이블명 fruit
+# 열이름 product(가변형 문자열 20자리), stock(정수형 숫자 3자리), price(정수형 숫자 10자리), receive(정수형 숫자 8자리), sweet(정수형 숫자 1자리)
+# 상품명(product), 수량(stock), 가격(price), 입고일자(receive), 당도(sweet)를 입력받아 테이블에 행을 추가한다.
+# 입고일자(receive):8자리(YYYYMMDD)로 입력해야함
+
+# 3. 판매(S)
+# 현재 테이블에 있는 상품리스트를 모두 보여준다.
+# 과일명(name)과 판매된 수량(sold)을 입력받아 해당되는 과일의 stock을 stock-sold값으로 변경(update)한다. 
+
+# 4. 재고리스트(L)
+# product(오름차순), stock(오름차순), receive(오름차순), sweet(내림차순) 에 따라 정렬 가능하게 한다.
+
+# 5. 품절품목확인(C)
+# 현재 재고(stock)가 0인 과일명을 테이블에서 가져와 보여준다.
+
+# 6. 삭제(D)
+# 현재 테이블에 있는 상품리스트를 보여준다.
+# 삭제하고 싶은 과일명(name)을 입력받아 테이블에서 해당되는 행을 삭제한다.
+
+# 7. 종료(Q)
+# 프로그램을 종료하면서 oracle과 연결을 닫는다.
+# (cursor.close() 와 conn.close())
+
 import cx_Oracle
 
 conn = cx_Oracle.connect('scott', 'tiger', 'localhost:1521/XE')
@@ -79,7 +109,6 @@ I - 입력 \t S - 판매 \t L- 재고 리스트 \t C-품절품목확인 \t D-삭
         sql = "delete from fruit where product = :1"
         name = input('삭제할 과일명 >>> ')
         cursor.execute(sql, (name,)) #tuple형태는 하나일 때 반점과 함께 써야 함
-
         conn.commit()
         
 
